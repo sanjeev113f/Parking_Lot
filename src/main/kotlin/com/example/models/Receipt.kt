9e026fc31becea57a8b54510393dbc79cba06870
sanjeev113f <sanjeevk@sahaj.ai>
2023-02-17 01:17:16 +0530

    Add receipt class model

---
+++ src/main/kotlin/com/example/models/Receipt.kt
@@ -0,0 +1,9 @@
+package com.example.models
+import java.util.*
+
+class Receipt {
+    private var receiptNumber = 1
+    private var entryDateTime: Date? = null
+    private var exitDateTime: Date? = null
+    private var fees: Int = 0
+}
